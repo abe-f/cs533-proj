b5e982fdc6b2acec4bde4d3132c0e7fbc0268e79
abe-f <afarrell538@gmail.com>
2024-04-15 14:30:01 -0400

    add capacity sensitivity study configs

--- run.py
+++ run.py
@@ -1,24 +1,54 @@
 import os
-# THIS FILE DOESN'T WORK YET
-# AT SOME POINT, USE THIS TO SPAWN A BUNCH OF JOBS
-# AND PUT THE DATA IN A UNIQUE FOLDER
-# STORE ONE CONFIG FOR EACH JOB (with the different command for the benchmark, and inputs)
+import subprocess
+import time
+import glob
+import shutil
 
-# keep trying to make data1, data2, etc. until one doesn't exist
-ctr = 1
-path = f"data{ctr}"
-while
-    ctr += 1
-    path = 
+# This script runs zsim jobs in parallel.
+# - The directory format for your experiments that this script expects is:
+# ./experiments/<experiment_name>/<config1>/<config1>.cfg
+#                               <config2>/<config2>.cfg
+#                               ...
+# where <config1>, <config2> can be anything but must be unique
 
-from multiprocessing import Pool
+# - The script will make a 'data' folder in config1, config2, etc that stores zsim's output for each experiment
+# - The cfg files themselves need to store the path to the benchmark used (with process0.command)
+# - Right now, only one experiment can be run, which has several top-level configs. In the future
+#       this can be extended to multiple experiments, each with multiple benchmark suites of configs
 
-def run_command(path):
-    command = "spark-submit driver.py -i {}".format(path)
-    subprocess.Popen(command, shell=True)
+# ./zsim/build/opt/zsim base_config.cfg
+top_dir = os.getcwd()
 
-pool = Pool()
-pool.map(run_command, paths)
+experiment = "capacity_sensitivity"
 
+if (not os.path.exists(f"experiments/{experiment}")):
+    print(f"Failure: experiments/{experiment} does not exist")
+    exit(0)
 
-# ./zsim/build/opt/zsim cs533/large.cfg --outputDir .
+config_folders = os.listdir(f"experiments/{experiment}")
+proc_dict = {}
+for config_folder in config_folders:
+    # Make data folder in the config folder and cd into it
+    # If it already exists, delete it (can change this in future)
+    os.chdir(f"{top_dir}/experiments/{experiment}/{config_folder}")
+    try:
+        os.mkdir(f"{top_dir}/experiments/{experiment}/{config_folder}/data")
+    except:
+        shutil.rmtree(f"{top_dir}/experiments/{experiment}/{config_folder}/data")
+        os.mkdir(f"{top_dir}/experiments/{experiment}/{config_folder}/data")
+    os.chdir(f"{top_dir}/experiments/{experiment}/{config_folder}/data")
+
+    # Change dir into the data folder and run the jobs
+    print(f"Changing working directory to: {top_dir}/experiments/{experiment}/{config_folder}/data")
+    command = f"../../../../zsim/build/opt/zsim ../{config_folder}.cfg"
+    print(f"Executing command: {command}")
+    proc_dict[f"{config_folder}"] = subprocess.Popen(command,  shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+while (bool(proc_dict.copy())):
+    for config in proc_dict.copy().keys():
+        if proc_dict[config].poll() is not None:
+            print(f"{config} ---> FINISHED")
+            proc_dict.pop(config)
+    time.sleep(10)
+ 
+print("All tests finished")
